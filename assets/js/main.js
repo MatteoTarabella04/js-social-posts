3e8dc0cc0cea6105b04ea276fcaa458787bdb91c
MatteoTarabella04 <tarabella.matteo04@gmail.com>
2023-03-24 16:52:42 +0100

    add function who create post footer markup

--- assets/js/main.js
+++ assets/js/main.js
@@ -87,6 +87,7 @@ posts.forEach(post => {
    createPostHeader(post);
    createPostText(post);
    createPostImage(post);
+   createPostFooter(post);
 });
 
 
@@ -141,5 +142,29 @@ function createPostImage(image) {
 
    console.log(postImgMarkup);
    return postImgMarkup;
+
+}
+
+// function create post footer
+function createPostFooter(postElements) {
+
+   const postFooterMarkup = `
+   <div class="post__footer">
+      <div class="likes js-likes">
+         <div class="likes__cta">
+            <a class="like-button js-like-button" href="#" data-postid="${postElements.id}">
+               <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+               <span class="like-button__label">Mi Piace</span>
+            </a>
+         </div>
+         <div class="likes__counter">
+            Piace a <b id="like-counter-1" class="js-likes-counter">${postElements.likes}</b> persone
+         </div>
+      </div>
+   </div>
+   `
+
+   console.log(postFooterMarkup);
+   return postFooterMarkup;
 
 }
